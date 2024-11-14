13e82127b165635bc441c1f22bd3653e2c8c8d8d
Ken Yeh <yehken@gmail.com>
2024-11-13 21:30:04 -0800

    score component

---
+++ src/components/Score.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Score({ date, score }) {
+  return (
+    <div className="score">
+      <p>Date: {date}</p>
+      <p>Score: {score}</p>
+    </div>
+  );
+}
+
+export default Score;
